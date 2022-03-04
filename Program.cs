8d0a533bdccae535a8a808fa337e8bb341d13460
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-03 22:42:43 -0300

    tarefa executada em sala de aula

--- Program.cs
+++ Program.cs
@@ -37,4 +37,42 @@ app.MapGet("/api/tarefas", ([FromServices] tarefasContext _db) =>
     return Results.Ok(_db.Tarefa.ToList<Tarefa>());
 });
 
+app.MapGet("/api/tarefas/{id}", ([FromServices]tarefasContext _db, [FromRoute] int id) =>
+{
+    var tarefa = _db.Tarefa.Find(id);
+
+    if (tarefa == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(tarefa);
+});
+
+app.MapPost("/api/tarefas",([FromServices] tarefasContext _db, [FromBody] Tarefa novaTarefa)=>
+{
+    if(String.IsNullOrEmpty(novaTarefa.Descricao))
+   {
+       return Results.BadRequest(new{mensagem = "Não é possivel cadastrar um nova tarefa sem descrição"});
+   }
+
+   if(novaTarefa.Concluida)
+   {
+       return Results.BadRequest(new{mensagem = "Não é possivel cadastrar uma nova tarefa, se a mesma já estiver concluida."});
+   }
+
+    var tarefa = new Tarefa
+    {
+        Descricao = novaTarefa.Descricao,
+        Concluida = novaTarefa.Concluida,
+    };
+    _db.Tarefa.Add(tarefa);
+    _db.SaveChanges();
+
+    string urlTarefaCriada = $"/api/tarefas/{tarefa.Id}";
+
+    return Results.Created(urlTarefaCriada, tarefa);
+
+});
+
 app.Run();
